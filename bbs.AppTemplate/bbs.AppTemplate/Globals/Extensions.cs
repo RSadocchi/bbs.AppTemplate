1ae7b5aa85f44833583d6fc2641d4520c6614dbe
RSado <r.sadocchi@outlook.it>
2018-05-03 19:10:53 +0200

    Custom attributes for enum

---
+++ bbs.AppTemplate/bbs.AppTemplate/Globals/Extensions.cs
@@ -0,0 +1,41 @@
+﻿using bbs.AppTemplate.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bbs.AppTemplate.Globals
+{
+    public static class Extensions
+    {
+        public static FontSize GetOrientationFontSize<Tsource>(this Tsource source) where Tsource : struct, IConvertible
+        {
+            if (!typeof(Tsource).IsEnum) return null;
+            Type attr = null;
+            var value = (FontSize)Attribute.GetCustomAttribute(attr, typeof(FontSize));
+            if (value == null) return null;
+            return value;
+        }
+
+        public static string GetStringValue<T>(this T source) where T : struct, IConvertible
+        {
+            if (!typeof(T).IsEnum) return null;
+            Type attr = null;
+            StringValue value = (StringValue)Attribute.GetCustomAttribute(attr, typeof(StringValue));
+            if (value == null) return null;
+            return value.Value;
+        }
+
+        public static TValue GetComplexValue<T, TValue>(this T sourcee)
+            where T : struct, IConvertible
+            where TValue : Type
+        {
+            if (!typeof(T).IsEnum) return null;
+            Type attr = null;
+            ComplexValue value = (ComplexValue)Attribute.GetCustomAttribute(attr, typeof(ComplexValue));
+            if (value == null) return null;
+            if (value.ValueType != null && value.ValueType != typeof(TValue))
+                throw new InvalidCastException($"Value is type of '{value.ValueType.GetType()}', cannot be casted in {typeof(TValue)}");
+            return (TValue)value.Value;
+        }
+    }
+}
---
+++ bbs.AppTemplate/bbs.AppTemplate/Services/CustomAttributeService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace bbs.AppTemplate.Services
+{
+    [AttributeUsage(AttributeTargets.Enum | AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
+    public class FontSize : Attribute
+    {
+        public double ExtraSmall { get; private set; }
+        public double Small { get; private set; }
+        public double Normal { get; private set; }
+        public double Large { get; private set; }
+        public double ExtraLarge { get; private set; }
+
+        public FontSize(double xsmall, double small, double normal, double large, double xlarge)
+        {
+            ExtraSmall = xsmall;
+            Small = small;
+            Normal = normal;
+            Large = large;
+            ExtraLarge = large;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Enum | AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
+    public class StringValue : Attribute
+    {
+        public string Value { get; private set; }
+        public StringValue(string value)
+        {
+            Value = value;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Enum | AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
+    public class ComplexValue : Attribute
+    {
+        public object Value { get; private set; }
+        public Type ValueType { get; private set; }
+        public ComplexValue(object value, Type type)
+        {
+            Value = value;
+            ValueType = type;
+        }
+    }
+}
